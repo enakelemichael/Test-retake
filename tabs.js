e1996bb1c41d4a1cd2b91840091c95adbdaca840
Enakelemichael <enakelemichael@yahoo.com>
2018-05-30 14:57:23 +0200

    done

--- tabs.js
+++ tabs.js
@@ -1,17 +1,39 @@
-function create_tabs(container_id) 
-{
+$menu = $('.menu');
 
-  const $pageA = $('.page');
-  const $headingA = $('h1').eq(0);
-  const $headingB = $('h1').eq(1);
-  const $headingC = $('h1').eq(2);
-  
-  $buttonA = $("<button id='btn-a'>" + $headingA + "</button>");
-  $buttonB = $("<button id='btn-b'>" + $headingB + "</button>");
-  $buttonC = $("<button id='btn-c'>" + $headingC + "</button>");
+const $pageA = $('.page').eq(0);
+const $pageB = $('.page').eq(1);
+const $pageC = $('.page').eq(2);
 
-  $menu.append($buttonA);
-  $menu.append($buttonB);
-  $menu.append($buttonC);
+const $headingA = $('h1').eq(0);
+const $headingB = $('h1').eq(1);
+const $headingC = $('h1').eq(2);
 
-}
+$buttonA = $("<button id='btn-a'> Content A </button>");
+$buttonB = $("<button id='btn-b'> Content B </button>");
+$buttonC = $("<button id='btn-c'> Content C </button>");
+
+
+$menu.append($buttonA);
+$menu.append($buttonB);
+$menu.append($buttonC);
+
+$pageB.hide();
+$pageC.hide();
+
+$buttonA.click(function(){
+    $pageA.show();
+    $pageB.hide();
+    $pageC.hide();
+});
+
+$buttonB.click(function(){
+    $pageA.hide();
+    $pageB.show();
+    $pageC.hide();
+});
+
+$buttonC.click(function(){
+    $pageA.hide();
+    $pageB.hide();
+    $pageC.show();
+});
